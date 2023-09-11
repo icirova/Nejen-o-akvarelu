6d0076bba6cc992f5299190bb6fe96ba189040fc
Iveta Círová <cirova.iveta@ptw.cz>
2023-09-11 15:36:27 +0200

    generování náhodných obrázků v sekci inspirace

---
+++ scripts/script.js
@@ -0,0 +1,190 @@
+const randomImages = [
+    {
+        link:"/random-gallery/1.webp",
+        alt:"peří",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/2.webp",
+        alt:"oko",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/3.webp",
+        alt:"lev",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/4.webp",
+        alt:"pozadí",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/5.webp",
+        alt:"pozadí",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/6.webp",
+        alt:"pozadí",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/7.webp",
+        alt:"kolo",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/8.webp",
+        alt:"přístav",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/9.webp",
+        alt:"dobroty",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/10.webp",
+        alt:"auto",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/11.webp",
+        alt:"auto",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/12.webp",
+        alt:"kočky",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/13.webp",
+        alt:"kočka",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/14.webp",
+        alt:"budka",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/15.webp",
+        alt:"květiny",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/16.webp",
+        alt:"kytka v květináči",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/17.webp",
+        alt:"kytice",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/18.webp",
+        alt:"dívka s květinou",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/19.webp",
+        alt:"dívka s deštníkem",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/20.webp",
+        alt:"hrozny",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/21.webp",
+        alt:"ozdoby",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/22.webp",
+        alt:"obličej s maskou",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/23.webp",
+        alt:"motorka",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+    {
+        link:"/random-gallery/24.webp",
+        alt:"les",
+        author_link:"https://pixabay.com/cs/users/ractapopulous-24766/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2561511",
+        author_name: "🎨JL G",
+    },
+];
+
+const getRandomImages = (images, count) => {
+    const selectedImages = [];
+    while (selectedImages.length < count) {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        const randomImage = images[randomIndex];
+        if (!selectedImages.includes(randomImage)) {
+            selectedImages.push(randomImage);
+        }
+    }
+    return selectedImages;
+};
+
+const selectedImages = getRandomImages(randomImages, 9);
+
+
+const sectionInspiration = document.getElementById("inspiration");
+
+selectedImages.forEach (function (img) {
+    console.log(img);
+    const divImageElement = document.createElement("div");
+    divImageElement.classList.add("image");
+    sectionInspiration.appendChild(divImageElement);
+
+    const imageElement = document.createElement("img");
+    imageElement.src = img.link;
+    imageElement.classList.add("image__img");
+    divImageElement.appendChild(imageElement);
+
+    const divDescriptionElement = document.createElement("div");
+    divDescriptionElement.classList.add("image__description");
+    divImageElement.appendChild(divDescriptionElement);
+
+    const paragraphAuthorElement = (document.createElement("p"));
+    paragraphAuthorElement.classList.add("image__author");
+    divDescriptionElement.appendChild(paragraphAuthorElement);
+
+    const linkElement = document.createElement("a");
+    linkElement.classList.add("link", "link--inspiration");
+    console.log(linkElement)
+    linkElement.href = img.author_link;
+    linkElement.textContent = img.author_name;
+    paragraphAuthorElement.appendChild(linkElement);
+});
